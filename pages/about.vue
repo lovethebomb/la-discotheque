0a0541c3dd81f5e7dfb51e413615269dc356ae90
lovethebomb <lucas@lovethebomb.eu>
2018-05-25 01:05:15 +0200

    About: add page

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="About">
+    <p><strong>La Discothèque</strong> is the lovechild of <a href="https://www.lucas.computer">Lucas Heymès</a> and <a href="https://www.inesdurao.me">Inês Durão</a></p>
+    <p>We decided to create this place so we could obnoxiously show-off how many cool records we have.</p>
+  </article>
+</template>
+
+<style>
+.About {
+  max-width: 345px;
+  text-align:center;
+  line-height: 21px;
+  margin: auto;
+}
+</style>
