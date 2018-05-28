4af513ed5b5cea3185e3543baf12bca7570cc891
lovethebomb <lucas@lovethebomb.eu>
2018-05-28 21:02:37 +0200

    Update logo with svg placeholder

--- components/Logo.vue
+++ components/Logo.vue
@@ -2,6 +2,7 @@
   <div class="Logo">
     <nuxt-link to="/">
       <h1>La Discothèque</h1>
+      <img src="/img/svg/logo-notfinal.svg">
     </nuxt-link>
   </div>
 </template>
@@ -9,5 +10,10 @@
 <style>
 .Logo a {
   color:inherit;
+}
+
+.Logo h1 {
+  visibility: hidden;
+  height: 0px;
 }
 </style>
