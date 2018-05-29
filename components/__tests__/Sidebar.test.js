2a245a608d387a5ebc14e6fa50d7ae4ba2ed7af1
lovethebomb <lucas@lovethebomb.eu>
2018-05-29 18:53:22 +0200

    Remove old Collection test, fix jest store for Sidebar

--- components/__tests__/Sidebar.test.js
+++ components/__tests__/Sidebar.test.js
@@ -1,15 +1,23 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+import createStore from '~/store'
 
 import NuxtLink from 'nuxt/lib/app/components/nuxt-link'
 import Sidebar from '~/components/Sidebar.vue'
 
+localVue.use(Vuex)
 describe('Sidebar.vue', () => {
+  let store
+
   beforeEach(() => {
+    store = createStore()
     Sidebar.components = Object.assign(Sidebar.components || {}, { 'NuxtLink': NuxtLink})
   })
 
   it('matches snapshot', () => {
-    const wrapper = shallowMount(Sidebar)
+    const wrapper = shallowMount(Sidebar, { store, localVue })
     expect(wrapper.html()).toMatchSnapshot()
   })
 })
